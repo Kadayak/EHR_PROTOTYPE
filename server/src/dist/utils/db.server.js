71193c10fb71ca7877b08137d5c113c4764c1481
kristoferfannar <kristoferfannarb@gmail.com>
2023-04-19 15:44:11 +0000

    works now

---
+++ server/src/dist/utils/db.server.js
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+let db;
+if (!global.__db) {
+    global.__db = new PrismaClient();
+}
+db = global.__db;
+export { db };
+//# sourceMappingURL=db.server.js.map
