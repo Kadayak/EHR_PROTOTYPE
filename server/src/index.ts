97aa2ccefbecffdd7f27812a977746a45e2b46e7
kristoferfannar <kristoferfannarb@gmail.com>
2023-05-18 17:02:21 +0200

    GET /medicalData/:id gets medical data from db
    medical data route /medicalData created with GET endpoint
    /medicalData/:id. Medical Data has one to one relationship with patient.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import * as dotenv from "dotenv";
 import logger from "./utils/logger.js";
 import cors from "cors";
+import { medicalDataRouter } from "./routes/medicalData/medicalData.router.js";
 import { patientRouter } from "./routes/patients/patients.router.js";
 import { doctorRouter } from "./routes/doctors/doctors.router.js";
 import { authRouter } from "./routes/auth/auth.router.js";
@@ -21,6 +22,7 @@ app.use(json());
 app.use(logger);
 app.use(cors());
 
+app.use("/api/medicalData", medicalDataRouter);
 app.use("/api/patients", patientRouter);
 app.use("/api/doctors", doctorRouter);
 app.use("/api/auth", authRouter);
@@ -28,8 +30,6 @@ app.use("/api/auth", authRouter);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
 
 app.listen(port, () => {
   console.log(`EHR app listening on port ${port}`);
---
+++ server/src/routes/medicalData/medicalData.service.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+
+import * as medicalDataRepository from "./medicalData.repository.js";
+
+export const getMedicalDataById = async (res: Response, id: string) => {
+  console.log("made it here");
+
+  const medicalData = await medicalDataRepository.getMedicalDataById(id);
+
+  if (!medicalData) {
+    return res
+      .status(404)
+      .json({ message: `medical data with id: ${id} not found` });
+  }
+
+  return res.status(200).json(medicalData);
+};
